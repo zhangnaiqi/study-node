68b1a764dbc4ba0754acac008c16fa7086cf7a7a
znq <1056623759@qq.com>
2019-10-16 18:17:16 +0800

    爬取豆瓣读书搜索javascript结果前10页数据

---
+++ douban-https-req-reptile.js
@@ -0,0 +1,64 @@
+const http = require("http");
+const https = require("https");
+const dexrpy = require("./douban-decrypt.js").decrypt;
+const writeFilePromise = require("./fileutils").writeFilePromise;
+let bookArr = [];
+
+let doubanPage = async page => {
+  console.log("page++++++++++++", page);
+
+  let url = https.get(
+    "https://search.douban.com/book/subject_search?search_text=javascript&cat=1001&start=" +
+      (page - 1) * 15,
+    res => {
+      let body = [];
+      res
+        .on("data", chunk => {
+          console.log("=====chunk", chunk);
+          body.push(chunk);
+        })
+        .on("end", async () => {
+          body = Buffer.concat(body).toString();
+
+          // console.log("body++++++", body);
+          //查找加密信息开始下标
+          const idx = body.indexOf('__DATA__ = "');
+          console.log("idx: ", idx);
+          //查找加密信息结束下标
+          const idx2 = body.indexOf('";');
+          console.log("idx2: ", idx2);
+          // 截取加密数据
+          let str = body.slice(idx + 12, idx2);
+          // console.log("str: ", str, "++++++++++++++++++");
+          // 解密数据
+          const bookList = dexrpy(str);
+          // 存储当前页json
+          const data = await writeFilePromise(
+            "douban.json",
+            JSON.stringify(bookList, null, " "),
+            "utf-8"
+          );
+          //遍历保存书名和其他信息
+          for (let i = 0; i < bookList.payload.items.length; i++) {
+            let boolItem = {
+              title: bookList.payload.items[i].title,
+              abstract: bookList.payload.items[i].abstract
+            };
+            bookArr.push(boolItem);
+          }
+          // console.log("bookArr=======", bookArr);
+          //爬取前10页数据
+          if (page < 10) {
+            doubanPage(page + 1);
+          } else {
+            const data1 = await writeFilePromise(
+              "doubanArr.js",
+              JSON.stringify(bookArr, null, " "),
+              "utf-8"
+            );
+          }
+        });
+    }
+  );
+};
+doubanPage(1);
---
+++ doubanArr.js
@@ -0,0 +1,606 @@
+[
+ {
+  "title": "[丛书] 你不知道的JavaScript",
+  "abstract": "人民邮电出版社 / 共3册 / 2018年出版"
+ },
+ {
+  "title": "JavaScript : The Good Parts",
+  "abstract": "Douglas Crockford / Yahoo Press / 2008-5 / GBP 23.99"
+ },
+ {
+  "title": "JavaScript高级程序设计（第3版）",
+  "abstract": "[美] Nicholas C. Zakas / 李松峰 / 曹力 / 人民邮电出版社 / 2012-3-29 / 99.00元"
+ },
+ {
+  "title": "JavaScript语言精粹",
+  "abstract": "Douglas Crockford / 赵泽欣 / 鄢学鹍 / 电子工业出版社 / 2009-4 / 35.00元"
+ },
+ {
+  "title": "JavaScript DOM编程艺术 （第2版）",
+  "abstract": "[英] Jeremy Keith / [加] Jeffrey Sambells / 杨涛 / 王建桥 / 杨晓云 等 / 人民邮电出版社 / 2011-4 / 49.00元"
+ },
+ {
+  "title": "JavaScript权威指南(第6版)",
+  "abstract": "David Flanagan / 淘宝前端团队 / 机械工业出版社 / 2012-4-1 / 139.00元"
+ },
+ {
+  "title": "你不知道的JavaScript（上卷）",
+  "abstract": "[美] Kyle Simpson / 赵望野 / 梁杰 / 人民邮电出版社 / 2015-4 / 49.00元"
+ },
+ {
+  "title": "高性能JavaScript",
+  "abstract": "Nicholas C.Zakas / 丁琛 / 赵泽欣 / 电子工业出版社 / 2010-11 / 49.00元"
+ },
+ {
+  "title": "JavaScript权威指南",
+  "abstract": "弗拉纳根 / 李强 / 机械工业出版社 / 2007-8 / 109.00元"
+ },
+ {
+  "title": "JavaScript设计模式与开发实践",
+  "abstract": "曾探 / 人民邮电出版社 / 2015-5 / 59.00元"
+ },
+ {
+  "title": "JavaScript DOM编程艺术",
+  "abstract": "Jeremy Keith / 杨涛 / 王建桥 / 杨晓云 / 人民邮电出版社 / 2006年12月 / 39.00元"
+ },
+ {
+  "title": "JavaScript语言精粹 : 修订版",
+  "abstract": "道格拉斯•克罗克福德 (Douglas Crockford) / 赵泽欣 / 鄢学鹍 / 电子工业出版社 / 2012-9-1 / 49.00元"
+ },
+ {
+  "title": "编写可维护的JavaScript",
+  "abstract": "扎卡斯 / 李晶 / 郭凯 / 张散集 / 人民邮电出版社 / 2013-4 / 55.00元"
+ },
+ {
+  "title": "JavaScript高级程序设计:第2版",
+  "abstract": "Nicholas Zakas / 李松峰 / 曹力 / 人民邮电出版社 / 2010-7 / 89.00元"
+ },
+ {
+  "title": "单页Web应用 : JavaScript从前端到后端",
+  "abstract": "米可夫斯基 (Michael S. Mikowski) / 鲍威尔 (Josh C. Powell) / 包勇明 / 人民邮电出版社 / 2014-9-1 / CNY 69.00"
+ },
+ {
+  "title": "JavaScript高级程序设计",
+  "abstract": "Nicholas C. Zakas / 曹力 / 张欣 / 人民邮电出版社 / 2006年9月 / 59.00元"
+ },
+ {
+  "title": "了不起的Node.js : 将JavaScript进行到底",
+  "abstract": "劳奇 (Guillermo Rauch) / 赵静 / 电子工业出版社 / 2014-1 / 79.00元"
+ },
+ {
+  "title": "JavaScript模式",
+  "abstract": "Stoyan Stefanov / 陈新 / 中国电力出版社 / 2012-7 / 38.00元"
+ },
+ {
+  "title": "Effective JavaScript : 编写高质量JavaScript代码的68个有效方法",
+  "abstract": "赫尔曼 (David Herman) / 黄博文 / 喻杨 / 机械工业出版社 / 2014-1-1 / CNY 49.00"
+ },
+ {
+  "title": "数据结构与算法JavaScript描述",
+  "abstract": "[美] Michael McMillan / 王群锋 / 杜　欢 / 人民邮电出版社 / 2014-8 / 49.00元"
+ },
+ {
+  "title": "基于MVC的JavaScript Web富应用开发",
+  "abstract": "麦卡劳(Alex MacCaw) / 李晶 / 张散集 / 电子工业出版社 / 2012-5 / 59.00元"
+ },
+ {
+  "title": "JavaScript忍者秘籍",
+  "abstract": "John Resig / Bear Bibeault / 徐涛 / 人民邮电出版社 / 2015-10 / 69.00"
+ },
+ {
+  "title": "JavaScript面向对象精要",
+  "abstract": "Nicholas C. Zakas / 胡世杰 / 人民邮电出版社 / 2015-4 / 39.00"
+ },
+ {
+  "title": "学习JavaScript数据结构与算法",
+  "abstract": "[巴西] 格罗纳（Loiane Groner） / 孙晓博 / 邓钢 / 吴双 / 陈迪 / 袁源 / 人民邮电出版社 / 2015-10-1 / 39.00"
+ },
+ {
+  "title": "JavaScript DOM高级程序设计",
+  "abstract": "Jeffrey Sambells / Aaron Gustafson / 李松峰 / 李雅雯 / 人民邮电出版社 / 2008-7 / 59.00元"
+ },
+ {
+  "title": "JavaScript设计模式",
+  "abstract": "Ross Harmes / Dustin Diaz / 谢廷晟 / 人民邮电出版社 / 2008 / 45.00元"
+ },
+ {
+  "title": "JavaScript & jQuery交互式Web前端开发",
+  "abstract": "[美]达克特(Duckett,J.) / 杜伟 / 柴晓伟 / 涂曙光 / 清华大学出版社 / 2015-6-9 / 79.80元"
+ },
+ {
+  "title": "Secrets of the JavaScript Ninja",
+  "abstract": "John Resig / Bear Bibeault / Manning Publications / 2013-1-17 / USD 16.99"
+ },
+ {
+  "title": "你不知道的JavaScript（中卷）",
+  "abstract": "[美] Kyle Simpson / 单业 / 姜南 / 人民邮电出版社 / 2016-8 / 79.00元"
+ },
+ {
+  "title": "你不知道的JavaScript（下卷）",
+  "abstract": "[美] Kyle Simpson / 单业 / 人民邮电出版社 / 2018-1-1 / 79.00"
+ },
+ {
+  "title": "JAVASCRIPT语言精髓与编程实践",
+  "abstract": "周爱民 / 电子工业出版社 / 2008-3 / 68.00元"
+ },
+ {
+  "title": "Head First HTML5 Programming（中文版） : 用JavaScript构建Web应用的初学者指南",
+  "abstract": "埃里克•弗里曼 (Eric Freeman) / 伊丽莎白•罗布森 (Elisabeth Robson) / 中国电力出版社 / 2012-9 / 78.00元"
+ },
+ {
+  "title": "Eloquent JavaScript (2 Edition) : A Modern Introduction to Programming",
+  "abstract": "Marijn Haverbeke / No Starch Press / 2014-11-25 / USD 39.95"
+ },
+ {
+  "title": "Three.js开发指南",
+  "abstract": "Jos Dirksen / 李鹏程 / 机械工业出版社 / 2015-3 / 59"
+ },
+ {
+  "title": "JavaScript设计模式",
+  "abstract": "张容铭 / 人民邮电出版社 / 2015-8-1 / 59.00"
+ },
+ {
+  "title": "JavaScript编程全解",
+  "abstract": "［日］井上诚一郎 / ［日］土江拓郎 / ［日］滨边将太 / 陈筱烟 / 人民邮电出版社 / 2013-12 / 79.00"
+ },
+ {
+  "title": "JavaScript编程精解(第2版)",
+  "abstract": "Marijn Haverbeke / 卢誉声 / 机械工业出版社 / 2016-1-1 / 79"
+ },
+ {
+  "title": "JavaScript面向对象编程指南 : 编写可扩展、可重用、高质量的JavaScript应用程序",
+  "abstract": "斯托扬 / 凌杰 / 人民邮电出版社 / 2013-3 / 59.00元"
+ },
+ {
+  "title": "JavaScript : The Good Parts",
+  "abstract": "克罗克福特 / 东南大学出版社 / 2009-1 / 28.00元"
+ },
+ {
+  "title": "JavaScript : The Definitive Guide",
+  "abstract": "David Flanagan / O'Reilly Media / 2011-5-13 / GBP 39.99"
+ },
+ {
+  "title": "HTML5+JavaScript动画基础",
+  "abstract": "兰贝塔 (Billy Lamberta) / 彼得 (Keith Peters) / 人民邮电出版社 / 69"
+ },
+ {
+  "title": "Node.js开发实战",
+  "abstract": "[美] Jim R. Wilson / 梅晴光 / 杜万智 / 陈琳 / 纪清华 / 段鹏飞 / 华中科技大学出版社 / 2018-11-10 / 99.90元"
+ },
+ {
+  "title": "JavaScript编程精解",
+  "abstract": "Marijn Haverbeke / 徐涛 / 机械工业出版社华章公司 / 2012-10-1 / 49.00元"
+ },
+ {
+  "title": "深入浅出JavaScript（中文版）",
+  "abstract": "Michael Morrison / O'Reilly Taiwan公司 / 东南大学出版社 / 2010-9 / 98.00元"
+ },
+ {
+  "title": "JavaScript 函数式编程",
+  "abstract": "[美] Michael Fogus / 欧阳继超 / 王妮 / 人民邮电出版社 / 2015-8 / 49"
+ },
+ {
+  "title": "DOM Scripting : Web Design with JavaScript and the Document Object Model",
+  "abstract": "Jeremy Keith / friendsofED / 2005-9-19 / USD 34.99"
+ },
+ {
+  "title": "JAVASCRIPT权威指南(第四版)",
+  "abstract": "David Flanagan / 张铭泽 / 等 / 机械工业出版社 / 2003-1-1 / 99.00"
+ },
+ {
+  "title": "JavaScript学习指南",
+  "abstract": "鲍尔斯 / 李荣青 / 吴兰陟 / 甲来安 / 人民邮电出版社 / 2009-10 / 45.00元"
+ },
+ {
+  "title": "精通JavaScript",
+  "abstract": "John Resig / 江疆 / 陈贤安 / 人民邮电出版社 / 2008-4-1 / 49.00元"
+ },
+ {
+  "title": "Effective JavaScript : 68 Specific Ways to Harness the Power of JavaScript",
+  "abstract": "David Herman / Addison-Wesley Professional / 2012-12-6 / USD 39.99"
+ },
+ {
+  "title": "ppk谈JavaScript",
+  "abstract": "Peter-Paul Koch / 淘宝UED / 人民邮电出版社 / 2008-4 / 59.00元"
+ },
+ {
+  "title": "Node : Up and Running: Scalable Server-Side Code with JavaScript",
+  "abstract": "Tom Hughes-Croucher / O'Reilly Media / 2012-5-7 / USD 34.99"
+ },
+ {
+  "title": "深入理解JavaScript",
+  "abstract": "Axel Rauschmayer / 王玉林 / 杜欢 / 庄婷婷 / 章子鹏 / 人民邮电出版社 / 2015-12 / 79"
+ },
+ {
+  "title": "Object-Oriented JavaScript : Create scalable, reusable high-quality JavaScript applications a",
+  "abstract": "Stoyan Stefanov / Packt Publishing / 2008-07-24 / USD 39.99"
+ },
+ {
+  "title": "Learning jQuery : Better Interaction Design and Web Development with Simple JavaSc",
+  "abstract": "Jonathan Chaffer / Karl Swedberg / Packt Publishing / 2007-7-7 / GBP 24.99"
+ },
+ {
+  "title": "Head First HTML5 Programming : Building Web Apps with JavaScript",
+  "abstract": "Eric Freeman / Elisabeth Robson / O'Reilly Media / 2011-10-18 / USD 49.99"
+ },
+ {
+  "title": "学习JavaScript数据结构与算法（第2版）",
+  "abstract": "[巴西] Loiane Groner / 邓　钢 / 孙晓博 / 吴　双 / 陈　迪 / 袁　源 / 人民邮电出版社 / 2017-9 / 49.00元"
+ },
+ {
+  "title": "Single Page Web Applications : JavaScript End-To-End",
+  "abstract": "Michael Mikowski / Josh Powell / Manning Publications / 2013-9-30 / USD 44.99"
+ },
+ {
+  "title": "JavaScript ES6函数式编程入门经典",
+  "abstract": "[印]Anto Aravinth / 梁宵 / 清华大学出版社 / 2017-11-28 / 49.80元"
+ },
+ {
+  "title": "CoffeeScript : Accelerated JavaScript Development",
+  "abstract": "Trevor Burnham / Pragmatic Bookshelf / 2011-9-29 / USD 29.00"
+ },
+ {
+  "title": "DOM Scripting : Web Design With Javascript and the Document Object Model",
+  "abstract": "Jeremy Keith / friendsofED / 2010-12 / GBP 27.50"
+ },
+ {
+  "title": "JavaScript异步编程 : 设计快速响应的网络应用",
+  "abstract": "Trevor Burnham / 许青松 / 人民邮电出版社 / 2013-6 / 32.00元"
+ },
+ {
+  "title": "动态网页制作 : Javascript",
+  "abstract": "伊格 / 上海交通大学出版社 / 2002-1 / 5.00元"
+ },
+ {
+  "title": "JavaScript : The Definitive Guide",
+  "abstract": "David Flanagan / O'Reilly Media / 2001-12-15 / USD 44.95"
+ },
+ {
+  "title": "JavaScript权威指南 第6版（上、下册） : JavaScript权威指南",
+  "abstract": "David Flanagan / 东南大学出版社 / 2011-1 / 128.00元"
+ },
+ {
+  "title": "JavaScript经典实例 : JavaScript经典实例",
+  "abstract": "Shelley Powers / 李强 / 中国电力出版社 / 2012-3 / 78.00元"
+ },
+ {
+  "title": "编写高质量代码 : 改善JavaScript程序的188个建议",
+  "abstract": "成林 / 机械工业出版社 / 2012-11 / 69.00元"
+ },
+ {
+  "title": "JavaScript : The Missing Manual",
+  "abstract": "David Sawyer McFarland / Pogue Press / 2008-7-1 / USD 39.99"
+ },
+ {
+  "title": "High Performance JavaScript : Build Faster Web Application Interfaces",
+  "abstract": "Nicholas C. Zakas / O'Reilly Media / 2010-4-2 / USD 34.99"
+ },
+ {
+  "title": "悟透JavaScript",
+  "abstract": "李战 / 电子工业出版社 / 2008-12 / 49.00元"
+ },
+ {
+  "title": "JavaScript王者归来",
+  "abstract": "月影 / 清华大学出版社 / 2008-7 / 86.00元"
+ },
+ {
+  "title": "Head First JavaScript",
+  "abstract": "Michael Morrison / O'Reilly Media / 2007-12-30 / GBP 30.99"
+ },
+ {
+  "title": "XMPP高级编程 : 使用JavaScript和jQuery",
+  "abstract": "[美] Jack Moffitt / 杨明军 / 清华大学出版社 / 2011-6 / 58.00元"
+ },
+ {
+  "title": "JavaScript框架设计",
+  "abstract": "司徒正美 / 人民邮电出版社 / 2014-4-9 / 89.00"
+ },
+ {
+  "title": "JavaScript语言精髓与编程实践",
+  "abstract": "周爱民 / 电子工业出版社 / 2012-3 / 79.00元"
+ },
+ {
+  "title": "全端Web开发 : 使用JavaScript与Java",
+  "abstract": "[美] Casimir Saternos / 王群锋 / 杜　欢 / 人民邮电出版社 / 2015-7 / 59.00元"
+ },
+ {
+  "title": "JavaScript Patterns",
+  "abstract": "Stoyan Stefanov / O'Reilly Media, Inc. / 2010-09-21 / USD 29.99"
+ },
+ {
+  "title": "Pro JavaScript Techniques",
+  "abstract": "John Resig / Apress / 2006-12-13 / USD 44.99"
+ },
+ {
+  "title": "JavaScript : The Missing Manual",
+  "abstract": "David Sawyer McFarland / O'Reilly Vlg. GmbH & Co. / 2008"
+ },
+ {
+  "title": "Understanding ECMAScript 6 : The Definitive Guide for JavaScript Developers",
+  "abstract": "Nicholas C. Zakas / No Starch Press / 2016-9-3 / USD 34.95"
+ },
+ {
+  "title": "Learning React Native : Building Native Mobile Apps with JavaScript",
+  "abstract": "Bonnie Eisenman / O'Reilly Media / 2015-12-25 / USD 39.99"
+ },
+ {
+  "title": "Eloquent JavaScript : A Modern Introduction to Programming",
+  "abstract": "Marijn Haverbeke / No Starch Press / 2011-2-3 / USD 29.95"
+ },
+ {
+  "title": "JavaScript设计模式",
+  "abstract": "Addy Osmani / 徐涛 / 人民邮电出版社 / 2013-6-1 / 49"
+ },
+ {
+  "title": "Simply JavaScript : Everyting You Need To Learn Javascript From Scratch",
+  "abstract": "Kevin Yank / Cameron Adams / SitePoint / 2007-6-21 / USD 39.95"
+ },
+ {
+  "title": "JavaScript面向对象编程指南（第2版）",
+  "abstract": "[加拿大] Stoyan Stefanov 斯托扬 著 / 陆禹淳 / 凌杰 / 人民邮电出版社 / 2014-12 / 69元"
+ },
+ {
+  "title": "使用HTML、CSS和JavaScript开发Android程序",
+  "abstract": "[美]Jonathan Stark / 申林 / 邹宇 / 电子工业出版社 / 2011-6 / 39.00元"
+ },
+ {
+  "title": "JavaScript: The Definitive Guide, 5th Edition : The Definitive Guide",
+  "abstract": "David Flanagan / O'Reilly Media / 2006-08-01 / USD 49.99"
+ },
+ {
+  "title": "测试驱动的JavaScript开发",
+  "abstract": "Christian Johansen / 赵勇 / 程德 / 凌杰 / 高博 / 机械工业出版社 / 2012-2-9 / 69.00元"
+ },
+ {
+  "title": "React : 引领未来的用户界面开发框架",
+  "abstract": "卓越开发者联盟 / 寸志 / 范洪春 / 杨森 / 陈涌 / 电子工业出版社 / 2015-5-1 / CNY 65.00"
+ },
+ {
+  "title": "JavaScript高效图形编程",
+  "abstract": "切克(Raffaele Cecco) / 人民邮电出版社 / 2012-5-1 / 45.00元"
+ },
+ {
+  "title": "Maintainable JavaScript : Writing Readable Code",
+  "abstract": "Nicholas C. Zakas / O'Reilly Media / 2012-6-22 / $34.99"
+ },
+ {
+  "title": "JavaScript权威指南 : 影印版第5版",
+  "abstract": "弗拉纳根 / 东南大学出版社 / 2007-6 / 99.00元"
+ },
+ {
+  "title": "Professional Node.js : Building Javascript Based Scalable Software",
+  "abstract": "Pedro Teixeira / Wrox / 2012-10-23 / USD 44.99"
+ },
+ {
+  "title": "Smashing Node.js : JavaScript Everywhere",
+  "abstract": "Guillermo Rauch / Wiley / 2012-5-1 / USD 44.99"
+ },
+ {
+  "title": "深入浅出CoffeeScript",
+  "abstract": "[英] Trevor Burnham / 寸志 / 人民邮电出版社 / 2012-5 / 35.00元"
+ },
+ {
+  "title": "Functional JavaScript : Introducing Functional Programming with Underscore.js",
+  "abstract": "Michael Fogus / O'Reilly Media / 2013-6-17 / USD 29.99"
+ },
+ {
+  "title": "Pro JavaScript Design Patterns",
+  "abstract": "Dustin Diaz / Ross Harmes / Apress / 2007-12-16 / USD 44.99"
+ },
+ {
+  "title": "ppk on JavaScript, 1/e",
+  "abstract": "Peter-Paul Koch / New Riders Press / 2006-09-20 / USD 44.99"
+ },
+ {
+  "title": "实战 ES2015 : 深入现代 JavaScript 应用开发",
+  "abstract": "小问 / 电子工业出版社 / 2016-10-1 / 79.00元"
+ },
+ {
+  "title": "Building Android Apps with HTML, CSS, and JavaScript : Android Apps With Html, Css, and Javascript",
+  "abstract": "Jonathan Stark / O'Reilly Media / 2010-10-4 / USD 29.99"
+ },
+ {
+  "title": "深入浅出JavaScript",
+  "abstract": "（德）海尔曼 / 牛海彬 / 人民邮电出版社 / 2008-04-01 / 55.00元"
+ },
+ {
+  "title": "JavaScript基础教程",
+  "abstract": "Tom Negrino / Dori Smith / 陈剑瓯 / 人民邮电出版社 / 2007-9 / 45.00元"
+ },
+ {
+  "title": "Node.js the Right Way : Practical, Server-Side JavaScript That Scales",
+  "abstract": "Jim R. Wilson / The Pragmatic Bookshelf / USD 17.00"
+ },
+ {
+  "title": "JavaScript : Visual QuickStart Guide",
+  "abstract": "Tom Negrino / Dori Smith / Peachpit Press / 2011-8-6 / USD 34.99"
+ },
+ {
+  "title": "JavaScript启示录",
+  "abstract": "Cody Lindley / 徐涛 / 人民邮电出版社 / 2014-3-1 / 35.00元"
+ },
+ {
+  "title": "Node即学即用",
+  "abstract": "[英] Tom Hughes-Croucher / [英] Mike Wilson / 郑达韡 / 人民邮电出版社 / 2013-2 / 39.00元"
+ },
+ {
+  "title": "JavaScript入门经典",
+  "abstract": "威尔顿(Paul Wilton) / 麦克匹克(Jeremy McPeak) / 张敏 / 高宇辉 / 王东亚 / 清华大学出版社 / 2011-2 / 88.00元"
+ },
+ {
+  "title": "Developing a React Edge : The JavaScript Library for User Interfaces",
+  "abstract": "Frankie Bagnardi / Jonathan Beebe / Richard Feldman / Tom Hallett / Simon Højberg / Karl Mikkelsen / Bleeding Edge Press / 2014-11 / USD 22.99"
+ },
+ {
+  "title": "Async JavaScript : Recipes for Event-Driven Code",
+  "abstract": "Trevor Burnham / CreateSpace / 2012-4-26 / USD 15.99"
+ },
+ {
+  "title": "Professional JavaScript for Web Developers",
+  "abstract": "Nicholas C. Zakas / Wrox / 2012-1-18 / USD 44.99"
+ },
+ {
+  "title": "JavaScript : The Definitive Guide",
+  "abstract": "David Flanagan / O'Reilly Media / 1997-1-8 / USD 32.95"
+ },
+ {
+  "title": "JavaScript入门经典 : 第4版",
+  "abstract": "蒙克尔 / 王军 / 人民邮电出版社 / 2007-3 / 39.00元"
+ },
+ {
+  "title": "Programming HTML5 Applications : Building Powerful Cross-Platform Environments in Javascript",
+  "abstract": "Zachary Kessin / O'Reilly Media / 2011-11-29 / USD 24.99"
+ },
+ {
+  "title": "JavaScript核心技术",
+  "abstract": "Shelley Powers / 苏敬凯 / 机械工业出版社 / 2007-6 / 45.00"
+ },
+ {
+  "title": "精通JavaScript+jQuery",
+  "abstract": "曾顺  编著 / 人民邮电出版社 / 2008-9 / 59.00元"
+ },
+ {
+  "title": "Professional JavaScript for Web Developers : Wrox Professional Guides",
+  "abstract": "Nicholas C. Zakas / Wrox / 2005-04-22 / USD 39.99"
+ },
+ {
+  "title": "Learning the Yahoo! User Interface library : Develop your next generation web applications with the YUI JavaS",
+  "abstract": "Dan Wellman / Packt Publishing / March 2008 / USD 44.99"
+ },
+ {
+  "title": "JavaScript核心概念及实践",
+  "abstract": "邱俊涛 / 人民邮电出版社 / 2013-5-1 / 49.00元"
+ },
+ {
+  "title": "Learning Web Design : A Beginner's Guide to HTML, CSS, JavaScript, and Web Graphics",
+  "abstract": "Robbins, Jennifer Niederst / O'Reilly Media / 2012-8-24 / USD 49.99"
+ },
+ {
+  "title": "JavaScript Web Applications",
+  "abstract": "Alex MacCaw / O'Reilly Media / 2011-8-30 / USD 34.99"
+ },
+ {
+  "title": "JavaScript : The Definitive Guide, 4th Edition (Definitive Guides)",
+  "abstract": "David Flanagan / Rebound by Sagebrush / 2001-11 / USD 59.35"
+ },
+ {
+  "title": "JavaScript : The Complete Reference, Second Edition",
+  "abstract": "POWELL / McGraw-Hill / 2004-6 / $ 56.49"
+ },
+ {
+  "title": "Javascript : Concepts & Techniques; Programming Interactive Web Sites",
+  "abstract": "Tina Spain McDuffie / Franklin Beedle & Associates / 2003-01 / USD 55.00"
+ },
+ {
+  "title": "JavaScript : The Good Parts ―「良いパーツ」によるベストプラクティス",
+  "abstract": "Douglas Crockford / オライリージャパン / 2008-12-22 / JPY 1890"
+ },
+ {
+  "title": "Javascript",
+  "abstract": "Gosselin, Don / 2010-3 / 918.00元"
+ },
+ {
+  "title": "JavaScript : Your visual blueprint  for building dynamic Web pages",
+  "abstract": "Kelly L. Murdock / Visual / 2000-7-20 / USD 24.99"
+ },
+ {
+  "title": "JavaScript : La guia definitiva/ The Definitive Guide",
+  "abstract": "David Flanagan / Anaya Multimedia / 2007-7-30 / USD 108.95"
+ },
+ {
+  "title": "JavaScript : The Web Technologies Series",
+  "abstract": "Don Gosselin / Course Technology / 2010-2-11 / USD 138.95"
+ },
+ {
+  "title": "HTML5游戏开发",
+  "abstract": "Jeanine Meyer / 徐阳 / 荆涛 / 人民邮电出版社 / 2011-10 / 49.00元"
+ },
+ {
+  "title": "Web Development with Node and Express : Leveraging the JavaScript Stack",
+  "abstract": "Ethan Brown / O'Reilly Media / 2014-7-25 / GBP 23.99"
+ },
+ {
+  "title": "深入浅出JavaScript",
+  "abstract": "莫里森 / 东南大学出版社 / 2008-8 / 98.00元"
+ },
+ {
+  "title": "JavaScript修炼之道",
+  "abstract": "波顿纽威 / 巩朋 / 张铁 / 人民邮电 / 2011-11 / 29.00元"
+ },
+ {
+  "title": "Speaking JavaScript",
+  "abstract": "Axel Rauschmayer / O'Reilly Media / 2014-3-21 / USD 39.99"
+ },
+ {
+  "title": "Mastering Dojo : JavaScript and Ajax Tools for Great Web Experiences",
+  "abstract": "Craig Riecke / Rawld Gill / Alex Russell / Pragmatic Bookshelf / 2008-7-5 / USD 38.95"
+ },
+ {
+  "title": "JavaScript 艺术与科学",
+  "abstract": "Cameron Adams，Jame Edwards，Chris Heilmann，Michael Mahemoff，Ara Pehlivanian，Dan Webb，Simon Willison / 郑文涛 / 电子工业出版社 / 2010年10月 / 49.00元"
+ },
+ {
+  "title": "JavaScript and JQuery : Interactive Front-end Web Development",
+  "abstract": "Jon Duckett / Wiley / 2014-1 / USD 39.99"
+ },
+ {
+  "title": "网页开发手记 : HTML+CSS+JavaScript实战详解",
+  "abstract": "叶青 / 电子工业 / 2011-6 / 65.00元"
+ },
+ {
+  "title": "JavaScript : The Complete Reference",
+  "abstract": "Thomas A. Powell / Fritz Schneider / McGraw-Hill/Osborne Media / 2001-09-20 / USD 49.99"
+ },
+ {
+  "title": "Javascript",
+  "abstract": "Pascarello, Eric / John Wiley & Sons Inc / 24.99"
+ },
+ {
+  "title": "JavaScript : A Programmer's Companion from Basic through DHTML, CSS and DOM",
+  "abstract": "Koch, Stefan / 2003-6 / 506.00元"
+ },
+ {
+  "title": "JavaScript",
+  "abstract": "Miller, Frederic P.; Vandome, Agnes F.; McBrewster, John"
+ },
+ {
+  "title": "JavaScript",
+  "abstract": "Olivier Hondermarck / Micro Application Editions / 2012-6-1 / EUR 20.50"
+ },
+ {
+  "title": "JavaScript",
+  "abstract": "Bucher Gruppe"
+ },
+ {
+  "title": "JavaScript",
+  "abstract": "Livres Groupe"
+ },
+ {
+  "title": "JavaScript",
+  "abstract": "Shelly, Gary B.; Cashman, Thomas J.; Dorin, William J. / 2000-8 / $ 126.50"
+ },
+ {
+  "title": "JavaScript",
+  "abstract": "Fuente Wikipedia"
+ },
+ {
+  "title": "JavaScript",
+  "abstract": "Source Wikipedia"
+ },
+ {
+  "title": "JavaScript",
+  "abstract": "Quelle Wikipedia"
+ },
+ {
+  "title": "JavaScript",
+  "abstract": "Sosu: Wikipedia"
+ },
+ {
+  "title": "JavaScript",
+  "abstract": "Istochnik Wikipedia"
+ },
+ {
+  "title": "JavaScript : Programming Basics for Absolute Beginners",
+  "abstract": "Nathan Clark / CreateSpace Independent Publishing Platform / 2016-11-26 / USD 16.38"
+ }
+]
